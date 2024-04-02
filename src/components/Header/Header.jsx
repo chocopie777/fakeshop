0edb4bd01e790ef4fc1d6edfd9aa5353f60f3733
chocopie777 <egoran99@gmail.com>
2024-04-02 16:05:34 +0500

    header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+
+export const Header = () => {
+  return (
+    <AppBar position="static">
+      <Container>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <ShoppingBagIcon sx={{ marginRight: 1 }} />
+              Fake Shop
+            </Box>
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
