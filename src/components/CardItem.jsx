b62978b27f18eab85d5a658375d1851ed0a24674
chocopie777 <egoran99@gmail.com>
2024-04-03 12:46:55 +0500

    carditem

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,6 +1,5 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { Button, Card, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
-
 export default function CardItem() {
     return (
         <>
@@ -8,15 +7,19 @@ export default function CardItem() {
                 <CardMedia
                     component="img"
                     height="194"
-                    image="/static/images/cards/paella.jpg"
+                    image="https://img.freepik.com/free-vector/modern-hosting-concept-with-flat-design_23-2147985997.jpg?w=740&t=st=1712128359~exp=1712128959~hmac=7112243f3f40934d080dc854854b778e300803e12509e2b126cbab405643d79d"
                     alt="Paella dish"
                 />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        This impressive paella is a perfect party dish and a fun meal to cook
-                        together with your guests. Add 1 cup of frozen peas along with the mussels,
-                        if you like.
+                <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography variant='body1'>
+                        Title
                     </Typography>
+                    <Typography variant='h6' component={'span'}>
+                        Price
+                    </Typography>
+                    <Button variant='contained' size='medium'>
+                            В корзину
+                    </Button>
                 </CardContent>
             </Card>
         </>
