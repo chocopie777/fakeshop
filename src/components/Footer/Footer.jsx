5d4aa9464949282e0ca77739bb1af1456b4423f6
chocopie777 <egoran99@gmail.com>
2024-04-03 20:59:41 +0500

    footer fix syntax error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer>
-            <Paper variant='outlined' elevation={3} square={true}>
+            <Paper variant='outlined' square={true}>
                 <Box sx={{ height: 100, display: 'flex', alignItems: 'center' }}>
                     <Container sx={{ display: 'flex', justifyContent: 'center' }}>
                         <Typography variant='h6' component='span'>
