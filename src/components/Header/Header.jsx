4683e9ed13f14bf42b08013538153f49858fd457
chocopie777 <egoran99@gmail.com>
2024-04-08 12:56:33 +0500

    drawer mui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
+import React, { useState } from 'react'
+import { AppBar, Box, Button, Container, Divider, Drawer, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+          <>
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+            <Divider  sx={{margin: '0 15px'}}/>
+          </>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <header>
       <AppBar position="static">
@@ -12,6 +36,17 @@ export const Header = () => {
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <ShoppingBagIcon sx={{ marginRight: 1 }} />
                 Fake Shop
+                <Button onClick={toggleDrawer(true)} variant="outlined" sx={{
+                  marginLeft: '20px', border: '1px solid #fff', color: '#fff', ":hover": {
+                    background: '#000', border: '1px solid #000',
+                  }
+                }}>
+                  <MenuIcon sx={{ marginRight: '10px' }} />
+                  Категории
+                </Button>
+                <Drawer open={open} onClose={toggleDrawer(false)}>
+                  {DrawerList}
+                </Drawer>
               </Box>
             </Typography>
           </Toolbar>
